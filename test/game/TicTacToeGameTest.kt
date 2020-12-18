48a0edac34f73e307a6402d242c132a51fa09d75
Stanislav Král <stanislav.kral3@gmail.com>
2020-12-18 12:49:12 +0100

    Added further testing of the tic tac toe game

--- test/game/TicTacToeGameTest.kt
+++ test/game/TicTacToeGameTest.kt
@@ -24,7 +24,7 @@ class TicTacToeGameTest {
     }
 
     @Test
-    fun setAddSeed2() {
+    fun testAddSeed2() {
         val g = TicTacToeGame(3, Seed.CROSS, 3)
 
         // it's not NOUGHT's turn
@@ -37,5 +37,31 @@ class TicTacToeGameTest {
         assert(g.state == TicTacToeGame.State.WON)
         assert(g.winner!! == Seed.CROSS)
         assert(g.victoriousCells == hashSetOf(g.board[0,0], g.board[1,1], g.board[2,2]))
+    }
+
+    @Test
+    fun testAddSeed3() {
+        val g = TicTacToeGame(3, Seed.CROSS, 3)
+        /*
+        0X0
+        XX0
+        X0X
+         */
+        assert(g.addSeed(0, 1, Seed.CROSS))
+        assert(g.addSeed(0, 0, Seed.NOUGHT))
+        assert(g.addSeed(1, 0, Seed.CROSS))
+        assert(g.addSeed(0, 2, Seed.NOUGHT))
+        assert(g.addSeed(1, 1, Seed.CROSS))
+        assert(g.addSeed(1, 2, Seed.NOUGHT))
+        assert(g.addSeed(2, 0, Seed.CROSS))
+        assert(g.addSeed(2, 1, Seed.NOUGHT))
+        assert(g.addSeed(2, 2, Seed.CROSS))
+
+        assert(!g.addSeed(2, 2, Seed.CROSS))
+        assert(!g.addSeed(2, 2, Seed.NOUGHT))
+
+        assert(g.state == TicTacToeGame.State.DRAW)
+        assert(g.winner == null)
+        assert(g.victoriousCells.size == 0)
     }
 }

621fb4d0437ccd545fc1b9b3f0891bd6de151c72
Stanislav Král <stanislav.kral3@gmail.com>
2021-01-04 08:09:40 +0100

    [CLIENT] Committed forgotten Pending.vue file

---
+++ client/components/game/Pending.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-row justify="center">
+    <v-col md="4">
+      <v-card>
+        <v-card-title>
+          Game lobby #{{ id }}
+        </v-card-title>
+
+        <v-card-subtitle v-if="opponentUsername !== undefined">
+          Please invite a user to play the game!
+        </v-card-subtitle>
+        <v-card-subtitle v-else>
+          {{opponentUsername}}
+        </v-card-subtitle>
+        <v-card-text>
+          <strong>Board size:</strong> {{ boardSize }}<br>
+          <strong>Victorious cells:</strong> {{ victoriousCells }}<br>
+          <div v-if="invitedUsers !== undefined && invitedUsers.length > 0">
+            <strong>Invited users:</strong><br>
+            <ul v-for="user in invitedUsers">
+             <li><span >{{user}}<br></span></li>
+            </ul>
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn text v-if="owner">START</v-btn>
+          <v-btn text
+                 @click="leave">LEAVE
+          </v-btn>
+        </v-card-actions>
+
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'Pending',
+  props: {
+    id: 0,
+    boardSize: Number,
+    victoriousCells: Number,
+    invitedUsers: Array,
+    owner: Boolean,
+    opponentUsername: String
+  },
+  methods: {
+    async leave() {
+      await this.$store.dispatch("game/leave")
+    }
+  }
+
+}
+</script>
+

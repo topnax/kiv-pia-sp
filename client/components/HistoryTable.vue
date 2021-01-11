98ae1a571311f6262685bd2d32bd92d13a98a625
Stanislav Král <stanislav.kral3@gmail.com>
2021-01-11 20:57:02 +0100

    [CLIENT] Moved history table to a separate component

---
+++ client/components/HistoryTable.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="games"
+    :items-per-page="5"
+    :loading="loading"
+    loading-text="Loading... Please wait"
+    class="elevation-1"
+  >
+    <template v-slot:item.won="{ item }">
+      <v-icon v-if="item.won" color="yellow">
+        mdi-trophy-variant
+      </v-icon>
+      <v-icon v-else color="error">
+        mdi-cancel
+      </v-icon>
+    </template>
+    <template v-slot:item.winner="{ item }">
+      <v-icon v-if="item.winner === 'X'">
+        mdi-close
+      </v-icon>
+      <v-icon v-else>
+        mdi-circle-outline
+      </v-icon>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  name: "HistoryList",
+
+  props: {
+    games: null,
+    loading: true,
+  },
+  data: () => ({
+    headers: [
+      {text: 'Won', value: 'won'},
+      {text: 'Opponent username', value: 'opponentUsername'},
+      {text: 'Winner', value: 'winner'},
+      {text: 'Board size', value: 'boardSize'},
+      {
+        text: 'Date',
+        align: 'start',
+        value: 'dateCreated',
+      },
+    ],
+  })
+}
+</script>
--- client/pages/history.vue
+++ client/pages/history.vue
@@ -1,31 +1,7 @@
 <template>
   <v-row>
     <v-col>
-      <v-data-table
-        :headers="headers"
-        :items="gamesParsed"
-        :items-per-page="5"
-        :loading="history.loading"
-        loading-text="Loading... Please wait"
-        class="elevation-1"
-      >
-        <template v-slot:item.won="{ item }">
-          <v-icon v-if="item.won" color="yellow">
-            mdi-trophy-variant
-          </v-icon>
-          <v-icon v-else color="error">
-            mdi-cancel
-          </v-icon>
-        </template>
-        <template v-slot:item.winner="{ item }">
-          <v-icon v-if="item.winner === 'X'">
-            mdi-close
-          </v-icon>
-          <v-icon v-else>
-            mdi-circle-outline
-          </v-icon>
-        </template>
-      </v-data-table>
+      <history-table :games="gamesParsed" :loading="history.loading"/>
     </v-col>
   </v-row>
 </template>
@@ -34,6 +10,9 @@
 import {mapGetters, mapState} from "vuex";
 
 export default {
+  components: {
+    HistoryTable: () => import('../components/HistoryTable'),
+  },
   mounted() {
     this.$store.dispatch("history/fetchHistory")
   },
@@ -43,19 +22,6 @@ export default {
       gamesParsed: 'history/gamesParsed',
     }),
   },
-  data: () => ({
-    headers: [
-      {text: 'Won', value: 'won'},
-      {text: 'Opponent username', value: 'opponentUsername'},
-      {text: 'Winner', value: 'winner'},
-      {text: 'Board size', value: 'boardSize'},
-      {
-        text: 'Date',
-        align: 'start',
-        value: 'dateCreated',
-      },
-    ],
-  }),
   layout: "user_loggedin",
   name: "history",
   middleware: ["auth"]

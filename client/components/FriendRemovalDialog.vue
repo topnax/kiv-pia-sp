1e6ae5d7791fad5695d49558bb357ec3fee1bee2
Stanislav Král <stanislav.kral3@gmail.com>
2021-01-14 19:39:44 +0100

    [CLIENT] Cancelling a friendship now brings up confirmation dialog

---
+++ client/components/FriendRemovalDialog.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-dialog
+    v-model="display"
+    persistent
+    max-width="290"
+  >
+    <v-card>
+      <v-card-title class="headline">
+        Do you really want to unfriend {{ friendUsername }}?
+      </v-card-title>
+      <v-card-text>This might hurt {{ friendUsername }}'s feelings...
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          text
+          @click="onCancelClicked"
+        >
+          No
+        </v-btn>
+        <v-btn
+          text
+          @click="onConfirmationClicked"
+        >
+          Yes
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "FriendRemovalDialog",
+  props: {
+    friendUsername: "",
+    onConfirmationClicked: null,
+    onCancelClicked: null,
+    display: false
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- client/pages/friends.vue
+++ client/pages/friends.vue
@@ -1,70 +1,85 @@
 <template>
-  <v-row class="pt-10" justify="center">
-    <v-col md="4" sm="12" cols="12">
-     <v-card
-       v-if="friendsLoading"
-     >
-       <v-card-text><v-progress-linear
-         indeterminate
-         color="teal"
-       ></v-progress-linear>
-       </v-card-text>
-     </v-card>
-
-
-      <div v-else>
-        <v-card v-if="friends.length === 0">
-          <v-card-text class="text-center">No friends found :(</v-card-text>
+  <v-container>
+    <friend-removal-dialog :display="displayRemovalDialog" :friend-username="friendToBeRemovedUsername"
+                           :on-cancel-clicked="removalCancelled" :on-confirmation-clicked="removalConfirmed"/>
+    <v-row class="pt-10" justify="center">
+      <v-col md="4" sm="12" cols="12">
+        <v-card
+          v-if="friendsLoading"
+        >
+          <v-card-text>
+            <v-progress-linear
+              indeterminate
+              color="teal"
+            ></v-progress-linear>
+          </v-card-text>
         </v-card>
-        <v-simple-table v-else>
-          <template v-slot:default>
-            <thead>
-            <tr>
-              <th class="text-left">
-                Username
-              </th>
-              <th class="text-right">
-              </th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr
+
+
+        <div v-else>
+          <v-card v-if="friends.length === 0">
+            <v-card-text class="text-center">No friends found :(</v-card-text>
+          </v-card>
+          <v-simple-table v-else>
+            <template v-slot:default>
+              <thead>
+              <tr>
+                <th class="text-left">
+                  Username
+                </th>
+                <th class="text-right">
+                </th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr
                 v-for="friend in friends"
                 :key="friend.id"
-            >
-              <td>{{ friend.username }}</td>
-              <td class="text-right">
-                <v-icon @click="cancelFriendship(friend.id)">mdi-cancel</v-icon>
-              </td>
-            </tr>
-            </tbody>
-          </template>
-        </v-simple-table>
-      </div>
-    </v-col>
-    <v-col class="text-center" md="4" sm="12" cols="12" v-if="requests.length !== 0">
-      <v-card
-        v-for="(request, i) in requests"
-        color="#385F73"
-        dark
-      >
-        <v-card-text class="text-left"><v-icon class="me-2">mdi-heart</v-icon><strong>{{ request.requestorUsername }}</strong> wants to become friends!</v-card-text>
-        <v-card-actions>
-          <v-btn text @click="acceptRequest(request.requestId)">Accept</v-btn>
-          <v-btn text @click="declineRequest(request.requestId)">Decline</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+              >
+                <td>{{ friend.username }}</td>
+                <td class="text-right">
+                  <v-icon @click="cancelFriendship(friend.id, friend.username)">mdi-cancel</v-icon>
+                </td>
+              </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+        </div>
+      </v-col>
+      <v-col class="text-center" md="4" sm="12" cols="12" v-if="requests.length !== 0">
+        <v-card
+          v-for="(request, i) in requests"
+          color="#385F73"
+          dark
+        >
+          <v-card-text class="text-left">
+            <v-icon class="me-2">mdi-heart</v-icon>
+            <strong>{{ request.requestorUsername }}</strong> wants to become friends!
+          </v-card-text>
+          <v-card-actions>
+            <v-btn text @click="acceptRequest(request.requestId)">Accept</v-btn>
+            <v-btn text @click="declineRequest(request.requestId)">Decline</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 import {mapState} from 'vuex'
+import FriendRemovalDialog from "@/components/FriendRemovalDialog";
 
 export default {
+  components: {FriendRemovalDialog},
   layout: "user_loggedin",
   name: "profile",
   middleware: ["auth"],
+  data: () => ({
+    friendToBeRemovedUsername: "",
+    displayRemovalDialog: false,
+    userIdToBeRemoved: -1,
+  }),
   computed: {
     ...mapState({
       requests: state => state.friendrequests.requests,
@@ -84,8 +99,17 @@ export default {
     declineRequest(requestId) {
       this.$store.dispatch("friendrequests/declineRequest", requestId)
     },
-    cancelFriendship(userId) {
-      this.$store.dispatch("friends/cancel", userId)
+    cancelFriendship(userId, username) {
+      this.userIdToBeRemoved = userId
+      this.friendToBeRemovedUsername = username
+      this.displayRemovalDialog = true
+    },
+    removalConfirmed() {
+      this.$store.dispatch("friends/cancel", this.userIdToBeRemoved)
+      this.displayRemovalDialog = false
+    },
+    removalCancelled() {
+      this.displayRemovalDialog = false
     }
   }
 }
